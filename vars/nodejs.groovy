b1d2387cc2480a5fe62f50fee9c3e3476b4e0cca
CodingManoj <mvp2612@hotmail.com>
2024-04-04 21:29:29 -0400

    Auto Push

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,10 +1,11 @@
 def lintchecks() {
     sh "echo Installing Lint Checker"
     sh "npm i jslint"
+    sh "echo Performing Lint Checks for $COMPONENT"
     sh "node_modules/jslint/bin/jslint.js server.js || true" 
 }
 
-def call() {
+def call(COMPONENT) {
     pipeline { 
         agent any
         stages {
