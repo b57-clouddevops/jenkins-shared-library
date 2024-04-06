7908ba411ef0d9a272b1fafce4ee9261eb66da6c
CodingManoj <mvp2612@hotmail.com>
2024-04-05 21:22:55 -0400

    Auto Push

--- vars/python.groovy
+++ vars/python.groovy
@@ -4,6 +4,14 @@ def lintchecks() {
         sh "echo Style Checks Completed $COMPONENT"
 }
 
+def sonarchecks() {
+    sh ''' 
+        echo Sonar Checks Starting for $COMPONENT
+        sonar-scanner  -Dsonar.host.url=http://172.31.80.115:9000 -Dsonar.sources=. -Dsonar.projectKey=user -Dsonar.login=admin -Dsonar.password=password
+        echo Sonar Checks Starting for $COMPONENT is Completed
+     '''
+}
+
 def call(COMPONENT) {
     pipeline { 
         agent any
@@ -18,7 +26,9 @@ def call(COMPONENT) {
 
             stage('Static Code Analysis') {
                 steps {
-                    sh "echo Static Checks ...."
+                    script {
+                        sonarchecks()
+                    }
                 }
             }
         }
