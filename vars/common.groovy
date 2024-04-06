2ce658dd98e573927d32c3b22581d3af1758db70
CodingManoj <mvp2612@hotmail.com>
2024-04-05 22:06:47 -0400

    Auto Push

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,7 +1,7 @@
 def sonarchecks() {
     sh ''' 
         echo Sonar Checks Starting for $COMPONENT
-        sonar-scanner  -Dsonar.host.url=http://{NEXUS_URL}:9000 ${ARGS} -Dsonar.projectKey=${COMPONENT} -Dsonar.login=admin -Dsonar.password=password
+        sonar-scanner  -Dsonar.host.url=http://${NEXUS_URL}:9000 ${ARGS} -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_CRED_USR} -Dsonar.password=${SONAR_CRED_PSW}
         echo Sonar Checks Starting for $COMPONENT is Completed
      '''
 }
