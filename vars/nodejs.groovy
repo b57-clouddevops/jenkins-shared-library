fae6f626ebed528775fe8d43376eec99a9f85592
CodingManoj <CodingManoj@gmail.com>
2024-04-12 21:23:06 -0400

    Auto Push

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -4,7 +4,9 @@ def call() {
         env.ARGS=" -Dsonar.sources=."
         common.sonarchecks
         common.testcases()
-        common.artifacts()
+        if(env.TAG_NAME != null) {
+            common.artifacts()
+        }
     }
 }
 
