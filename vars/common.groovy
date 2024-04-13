336b79cb16caf199f244d91640449ad0aeef6167
CodingManoj <CodingManoj@gmail.com>
2024-04-12 21:36:02 -0400

    Auto Push

--- vars/common.groovy
+++ vars/common.groovy
@@ -9,7 +9,7 @@ def lintchecks() {
 }
 
 def sonarchecks() {
-    stage {
+    stage('Sonar Checks') {
         sh ''' 
             echo Sonar Checks Starting for ${COMPONENT}
             # sonar-scanner  -Dsonar.host.url=http://${NEXUS_URL}:9000 ${ARGS} -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_CRED_USR} -Dsonar.password=${SONAR_CRED_PSW}
@@ -19,7 +19,7 @@ def sonarchecks() {
 }
 
 def sonarresult() {
-    stage {
+    stage('Sonar Result') {
         sh '''
                 curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > gate.sh
                 # bash -x gate.sh ${SONAR_CRED_USR} ${SONAR_CRED_PSW} ${NEXUS_URL} ${COMPONENT} ||true
