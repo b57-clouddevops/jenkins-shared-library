4900aa7520dab94460c9c57c54191411f07f193c
CodingManoj <CodingManoj@gmail.com>
2024-04-12 21:43:01 -0400

    Auto Push

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,5 +1,6 @@
 def call() {
     node {
+        git branch: 'main', url: 'https://github.com/b57-clouddevops/cart.git'
         common.lintchecks()
         env.ARGS="-Dsonar.sources=."
         common.sonarchecks()
