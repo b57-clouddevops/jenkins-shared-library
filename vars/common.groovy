4d4be1ceabb53e78d33ff14f0456cbe44c172cc7
CodingManoj <CodingManoj@gmail.com>
2024-04-12 21:11:33 -0400

    Auto Push

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,9 +1,21 @@
+def lintchecks() {
+    stage('Sonar Checks') {
+        sh '''
+            echo Installing Lint Checker
+            echo Performing Lint Checks for $COMPONENT
+            echo Lint Checks Completed for $COMPONENT
+        ''' 
+    }
+}
+
 def sonarchecks() {
-    sh ''' 
-        echo Sonar Checks Starting for $COMPONENT
-        # sonar-scanner  -Dsonar.host.url=http://${NEXUS_URL}:9000 ${ARGS} -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_CRED_USR} -Dsonar.password=${SONAR_CRED_PSW}
-        echo Sonar Checks Starting for $COMPONENT is Completed
-     '''
+    stage {
+        sh ''' 
+            echo Sonar Checks Starting for $COMPONENT
+            # sonar-scanner  -Dsonar.host.url=http://${NEXUS_URL}:9000 ${ARGS} -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_CRED_USR} -Dsonar.password=${SONAR_CRED_PSW}
+            echo Sonar Checks Starting for $COMPONENT is Completed
+        '''
+    }
 }
 
 def sonarresult() {
@@ -37,7 +49,22 @@ def testcases() {
     }
 }
 
-def checkrelease() {
-    env.upload_status=sh(returnStdout: true, script: "curl -s -L http://172.31.38.109:8081/service/rest/repository/browse/${COMPONENT}/ | grep ${COMPONENT}-${TAG_NAME}.zip || true")
-    print upload_status
-}
+
+def artifacts() {
+    stage('Checkting Artifact On Nexus') {
+        env.upload_status=sh(returnStdout: true, script: "curl -s -L http://172.31.38.109:8081/service/rest/repository/browse/${COMPONENT}/ | grep ${COMPONENT}-${TAG_NAME}.zip || true")
+        print upload_status
+    }
+    
+    if(env.upload_status == "") {
+        stage('Generate Artifacts') {
+
+        }
+
+        stage('Publish Artifacts') {
+            
+            }
+        }
+    }
+
+
